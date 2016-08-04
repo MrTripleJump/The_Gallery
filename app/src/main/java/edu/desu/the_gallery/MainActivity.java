4e37b3b4ac459251232ec460a70a618f2f7cd3a5
dsuappacademy <mrtj48@gmail.com>
2016-08-04 10:48:55 -0400

    added another category of art, main artist page and email intent button.

--- app/src/main/java/edu/desu/the_gallery/MainActivity.java
+++ app/src/main/java/edu/desu/the_gallery/MainActivity.java
@@ -1,15 +1,27 @@
 package edu.desu.the_gallery;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.StringDef;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    ViewPager viewPager;
-    CustomSwipeAdapter adapter;
-     int[] image_resource;
-     int[] caption;
-    int[] caption1;
+    private ViewPager viewPager;
+    private CustomSwipeAdapter adapter;
+    private int[] image_resource;
+    private int[] caption;
+    private int[] caption1;
+    private ShareActionProvider mShareActionProvider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +29,43 @@ public class MainActivity extends AppCompatActivity {
 
         image_resource = new int[] {R.drawable.skyline, R.drawable.independence2, R.drawable.theater, R.drawable.trainstation};
         caption = new int[] {R.string.skyline_summary, R.string.hall_summary, R.string.theater_summary, R.string.station_summary};
-        caption1 = new int[] {R.string.summary3, R.string.summary3, R.string.summary3, R.string.summary3};
+        caption1 = new int[] {R.string.summary1, R.string.summary1, R.string.summary1, R.string.summary1};
 
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         adapter = new CustomSwipeAdapter(this, caption, image_resource, caption1);
-        viewPager.setAdapter(adapter);
+        viewPager.setAdapter(adapter);}
+
+            public void contact(View target) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Im interested in your work ");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }// Do stuff
+            }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.share, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        // Return true to display menu
+        return true;
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
 
     }
-}
+
---
+++ app/src/main/java/edu/desu/the_gallery/mainPage.java
@@ -0,0 +1,33 @@
+package edu.desu.the_gallery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+public class mainPage extends AppCompatActivity {
+
+    final Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        ImageView profile = (ImageView) findViewById(R.id.imageView);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent freeIntent = new Intent(context, Selection.class);
+
+                startActivity(freeIntent);
+
+            }
+
+        });
+
+    }
+}
